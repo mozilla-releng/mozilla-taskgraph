aa37b3dce4ab40a1c4d53cd0bcb2edd56019e62d
Andrew Halberstadt <ahal@mozilla.com>
2024-01-11 16:32:03 -0500

    feat(shipit): allow 'mark-as-shipped' to define product in task

--- src/mozilla_taskgraph/transforms/scriptworker/shipit/mark_as_shipped.py
+++ src/mozilla_taskgraph/transforms/scriptworker/shipit/mark_as_shipped.py
@@ -3,34 +3,74 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import os
+from textwrap import dedent
 
 from taskgraph.transforms.base import TransformSequence
+from taskgraph.util.schema import Schema, optionally_keyed_by, resolve_keyed_by
+from voluptuous import Extra, Optional
 
 transforms = TransformSequence()
+
+mark_as_shipped_schema = Schema(
+    {
+        Optional("shipit-product"): optionally_keyed_by("build-type", str),
+        Extra: object,
+    }
+)
+
+transforms.add_validate(mark_as_shipped_schema)
+
+
+@transforms.add
+def resolve_keys(config, tasks):
+    for task in tasks:
+        resolve_keyed_by(
+            task,
+            "shipit-product",
+            item_name=task.get("name", "mark-as-shipped"),
+            **{
+                "build-type": task.get("attributes", {}).get("build-type"),
+            },
+        )
+        yield task
 
 
 @transforms.add
 def make_task_description(config, tasks):
-    if "shipit" not in config.graph_config:
-        config_path = os.path.join(config.graph_config.root_dir, "config.yml")
-        raise Exception(f"Missing 'shipit' config in {config_path}")
+    shipit = {}
+    if "shipit" in config.graph_config:
+        shipit = config.graph_config["shipit"]
 
-    shipit = config.graph_config["shipit"]
     release_format = shipit.get(
         "release-format", "{product}-{version}-build{build_number}"
     )
     scope_prefix = shipit.get("scope-prefix", "project:releng:ship-it")
     shipit_server = "production" if config.params["level"] == "3" else "staging"
     version = config.params.get("version", "<ver>")
+
     for task in tasks:
+        product = task.pop("shipit-product", shipit.get("product"))
+
+        if not product:
+            config_path = os.path.join(config.graph_config.root_dir, "config.yml")
+            raise Exception(
+                dedent(
+                    f"""
+                Can't determine ShipIt product!
+
+                Define it as `shipit-product` in the task definition, or as `shipit.product`
+                in {config_path}.""".lstrip()
+                )
+            )
+
         task.setdefault("label", "mark-as-shipped")
-        task["description"] = f"Mark {shipit['product']} as shipped in Ship-It"
+        task["description"] = f"Mark {product} as shipped in Ship-It"
         task["scopes"] = [
             f"{scope_prefix}:action:mark-as-shipped",
             f"{scope_prefix}:server:{shipit_server}",
         ]
         task.setdefault("worker", {})["release-name"] = release_format.format(
-            product=shipit["product"],
+            product=product,
             version=version,
             build_number=config.params.get("build_number", 1),
         )
