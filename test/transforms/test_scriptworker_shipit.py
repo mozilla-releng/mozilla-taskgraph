6457fce9c41fc3b8fb4515558a65d78a7bcaeb51
Andrew Halberstadt <ahal@mozilla.com>
2024-01-11 16:32:03 -0500

    feat(shipit): allow 'mark-as-shipped' to define product in task

--- test/transforms/test_scriptworker_shipit.py
+++ test/transforms/test_scriptworker_shipit.py
@@ -8,7 +8,7 @@ from mozilla_taskgraph.transforms.scriptworker.shipit.mark_as_shipped import (
 )
 
 
-def assert_missing_config(e):
+def assert_missing_product(e):
     assert isinstance(e, Exception)
 
 
@@ -47,18 +47,45 @@ def assert_scope_prefix(task):
     ]
 
 
+def assert_product_in_task(task):
+    assert task == {
+        "description": "Mark my-app as shipped in Ship-It",
+        "label": "mark-as-shipped",
+        "scopes": [
+            "project:releng:ship-it:action:mark-as-shipped",
+            "project:releng:ship-it:server:production",
+        ],
+        "worker": {"release-name": "my-app-1.0.0-build1"},
+    }
+
+
+def assert_product_in_task_keyed_by(task):
+    assert task == {
+        "attributes": {"build-type": "linux"},
+        "description": "Mark my-app-linux as shipped in Ship-It",
+        "label": "mark-as-shipped",
+        "scopes": [
+            "project:releng:ship-it:action:mark-as-shipped",
+            "project:releng:ship-it:server:production",
+        ],
+        "worker": {"release-name": "my-app-linux-1.0.0-build1"},
+    }
+
+
 @pytest.mark.parametrize(
-    "shipit_config,params",
+    "shipit_config,params,task",
     (
-        pytest.param({}, None, id="missing_config"),
+        pytest.param({}, {"version": "1.0.0", "level": "3"}, {}, id="missing_product"),
         pytest.param(
             {"shipit": {"product": "app"}},
             {"version": "1.0.0", "level": "1"},
+            {},
             id="default_level_1",
         ),
         pytest.param(
             {"shipit": {"product": "app"}},
             {"version": "1.0.0", "level": "3"},
+            {},
             id="default_level_3",
         ),
         pytest.param(
@@ -69,12 +96,34 @@ def assert_scope_prefix(task):
                 }
             },
             {"level": "1", "version": "1.2.3", "build_number": "10"},
+            {},
             id="release_format",
         ),
         pytest.param(
             {"shipit": {"product": "app", "scope-prefix": "project:app:releng"}},
             {"level": "1"},
+            {},
             id="scope_prefix",
+        ),
+        pytest.param(
+            {},
+            {"version": "1.0.0", "level": "3"},
+            {"shipit-product": "my-app"},
+            id="product_in_task",
+        ),
+        pytest.param(
+            {},
+            {"version": "1.0.0", "level": "3"},
+            {
+                "attributes": {"build-type": "linux"},
+                "shipit-product": {
+                    "by-build-type": {
+                        "linux": "my-app-linux",
+                        "default": "my-app-windows",
+                    }
+                },
+            },
+            id="product_in_task_keyed_by",
         ),
     ),
 )
@@ -85,12 +134,13 @@ def test_mark_as_shipped(
     run_transform,
     shipit_config,
     params,
+    task,
 ):
     graph_config = make_graph_config(extra_config=shipit_config)
     config = make_transform_config(graph_cfg=graph_config, params=params)
 
     try:
-        result = run_transform(mark_as_shipped_transforms, {}, config)
+        result = run_transform(mark_as_shipped_transforms, task, config)
         assert len(result) == 1
         result = result[0]
 
