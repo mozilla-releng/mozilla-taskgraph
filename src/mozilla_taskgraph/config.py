a612c108d1cbd2609bde31cb969d66836943bf8c
Andrew Halberstadt <ahal@mozilla.com>
2024-01-18 11:35:34 -0500

    fix(shipit): make 'shipit.product' optional in graph_config schema
    This was an omission that should have been included with
    6457fce9c41fc3b8fb4515558a65d78a7bcaeb51.

--- src/mozilla_taskgraph/config.py
+++ src/mozilla_taskgraph/config.py
@@ -1,10 +1,10 @@
 from taskgraph import config as tg
-from voluptuous import Optional, Required
+from voluptuous import Optional
 
 tg.graph_config_schema = tg.graph_config_schema.extend(
     {
         Optional("shipit"): {
-            Required("product"): str,
+            Optional("product"): str,
             Optional("release-format"): str,
             Optional("scope-prefix"): str,
         },
