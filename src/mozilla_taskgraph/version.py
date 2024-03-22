af318bb46b0a405c18e3d3d9cc207a1720bb4011
Julien Cristau <jcristau@mozilla.com>
2024-03-22 10:09:11 +0100

    fix: look for version.txt in cwd, not where mozilla-taskgraph is installed

--- src/mozilla_taskgraph/version.py
+++ src/mozilla_taskgraph/version.py
@@ -2,11 +2,9 @@ import os
 
 from taskgraph.util.vcs import get_repository
 
-here = os.path.abspath(os.path.dirname(__file__))
-
 
 def default_parser(params):
-    repo_root = get_repository(here).path
+    repo_root = get_repository(os.getcwd()).path
 
     with open(os.path.join(repo_root, "version.txt")) as f:
         return f.read().strip()
