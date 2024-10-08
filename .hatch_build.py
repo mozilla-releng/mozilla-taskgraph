aaa49c7880d8f0b684093551e9ce2607c3c28726
Andrew Halberstadt <ahal@mozilla.com>
2024-10-08 09:43:20 -0400

    chore: set version in pyproject.toml
    While feasible to keep it in version.txt, it causes extra work for tools
    like hatchling and commitizen. I can't think of any benefit of keeping
    this in version.txt, so let's just move it to the standard location.

--- .hatch_build.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-from hatchling.metadata.plugin.interface import MetadataHookInterface
-
-
-class MetaDataHook(MetadataHookInterface):
-    def update(self, metadata):
-        metadata["version"] = Path(self.root).joinpath("version.txt").read_text()
